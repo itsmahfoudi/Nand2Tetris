0535d464289482d3b45b91b0142478d38abd941c
= <ayoubelmahfoudi05@gmail.com>
2024-04-29 15:23:26 +0100

    Add implementation for the FILL.asm program.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,41 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+    // Set off = 8191
+    @8191
+    D=A 
+    @off
+    M=D
+(MAIN)
+    @KBD
+    D=M
+    @SET_VAL
+    D;JEQ
+    D=-1
+    0;JMP
+
+(SET_VAL)
+    @val 
+    M=D
+    @FILL 
+    0;JMP 
+
+(FILL)
+    @off
+    D=M 
+    @SCREEN 
+    D=D+A 
+    @val 
+    M=D // Set M to point to @screen + offset
+    D=A // Set D to Val
+    M=D // Set M = val
+    @off
+    D=M 
+    D=D-1
+    M=D
+    @MAIN
+    D;JLT
+    @FILL
+    0;JMP
+    
+    
